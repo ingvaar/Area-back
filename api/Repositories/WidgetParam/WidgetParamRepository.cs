1db802f52db841c2beb8d028f0c5f51797460120
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2020-02-11 13:53:33 +0100

    Creating WidgetParamRepository

---
+++ api/Repositories/WidgetParam/WidgetParamRepository.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using area.Contexts;
+using area.Models.WidgetParam;
+using area.Repositories.Widget;
+using Microsoft.EntityFrameworkCore;
+
+namespace area.Repositories.WidgetParam
+{
+    public class WidgetParamRepository : IWidgetParamRepository
+    {
+        private readonly DbSet<WidgetParamModel> _repository;
+        private readonly WidgetRepository _widgetRepository;
+        
+        public WidgetParamRepository(AreaContext context)
+        {
+            _repository = context.WidgetParam;
+            _widgetRepository = new WidgetRepository(context);
+        }
+        
+        public WidgetParamModel[] GetWidgetParams(int offset, int limit)
+        {
+			return _repository.OrderBy(p => p.Id)
+				.Skip(offset)
+				.Take(limit)
+				.ToArray();
+        }
+
+        public WidgetParamModel GetWidgetParamByWidgetId(int widgetId)
+        {
+            var id = _widgetRepository.GetWidgetById(widgetId).ParamId;
+            return _repository.SingleOrDefault(p => p.Id == id);
+        }
+
+        public WidgetParamModel GetWidgetParamById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
